182005ea1ee5f88c539ac5484510affb81efbbd3
yeon119 <128660797+yeon119@users.noreply.github.com>
2026-04-06 16:00:53 +0900

    Add files via upload

--- 202344070/C_DS/ArrayList.py
+++ 202344070/C_DS/ArrayList.py
@@ -5,19 +5,19 @@ class ArrayList:
         self.array = [None] * capacity
         
     def isEmpty(self):
-        return self.size == 0
+        return self.size == 0 # 비어있는데 삭제하려면 에러가 나기떄문에 언더플로우 방지
     
     def isFull(self):
-        return self.size == self.capacity
+        return self.size == self.capacity # 가득차있는데 넣을려고 하면 에러남 오버플로우 방지
     
     def getEntry(self,pos):
-        if 0<= pos<self.size:
+        if 0<= pos<self.size: 
             return self.array[pos]
         else:
             return None
     
     def insert(self,pos,e):
-        if not self.isFull() and 0 <= pos <=self.size:
+        if not self.isFull() and 0 <= pos <= self.size:
             for i in range(self.size,pos, -1):
                 self.array[i] = self.array[i-1]
             self.array[pos] = e
@@ -34,8 +34,8 @@ class ArrayList:
         else:
             pass
         
-    def __str__(self):
-        return str(self.array[0:self.size])
+    def __str__(self): # callback method array를 출력하는데 다양한 문자형이 있는데 우리는 보기 위함이기 떄문에 다 문자열로 바꿈
+        return str(self.array[0:self.size]) # 슬라이싱
     
     def replace(self,pos,e):
         if not self.isEmpty() and 0 <=pos <self.size:
@@ -43,7 +43,7 @@ class ArrayList:
         else:
             pass
         
-    def count(self,e):
+    def count(self,e): 
         cnt = 0
         for i in range(0,self.size):
             if self.array[i] == e:
